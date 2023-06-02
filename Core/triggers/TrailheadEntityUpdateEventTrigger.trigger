e24c271b41c339a5bcdd1572ad37b1cafa421135
Ben <blearn5241@gmail.com>
2023-06-02 12:25:25 -0400

    progress on issue #1 (finished current implementation of after update badge/trail triggers)

--- Core/triggers/TrailheadEntityUpdateEventTrigger.trigger
+++ Core/triggers/TrailheadEntityUpdateEventTrigger.trigger
@@ -1,10 +1,3 @@
-/**
- * @description       : 
- * @author            : Ben Learn
- * @group             : 
- * @last modified on  : 06-02-2023
- * @last modified by  : Ben Learn
-**/
 trigger TrailheadEntityUpdateEventTrigger on TrailheadEntityUpdateEvent__e (after insert) {
-
+    new TrailheadEntityUpdateEventTriggerHandler().execute();
 }
--- documentation/assets/search-idx.js
+++ documentation/assets/search-idx.js
@@ -2,26 +2,26 @@ export default [
     {
         "title": "Home",
         "fileName": "index.html",
-        "text": "Home hello"
+        "text": "Home Trail Tracker Provider Check out the Recommended Badges wiki !"
     },
     {
         "title": "BadgeTriggerHandler",
         "fileName": "BadgeTriggerHandler.html",
-        "text": "BadgeTriggerHandler : Signature public without sharing class BadgeTriggerHandler extends TriggerHandler Author : Ben Learn BadgeTriggerHandler Constructors BadgeTriggerHandler() BadgeTriggerHandler() Signature public BadgeTriggerHandler() BadgeTriggerHandler Methods afterUpdate(updatedRecordsMap, oldRecordsMap) afterUpdate(updatedRecordsMap, oldRecordsMap) Signature protected override void afterUpdate(Map<Id, sObject> updatedRecordsMap, Map<Id, sObject> oldRecordsMap)"
+        "text": "BadgeTriggerHandler Trigger handler for trailheadapp__Badge__c custom object Signature public without sharing class BadgeTriggerHandler extends TriggerHandler Author Ben Learn Since 06-01-2023 BadgeTriggerHandler Constructors BadgeTriggerHandler() BadgeTriggerHandler() Signature public BadgeTriggerHandler() BadgeTriggerHandler Methods afterUpdate(updatedRecordsMap, oldRecordsMap) afterUpdate(updatedRecordsMap, oldRecordsMap) Signature protected override void afterUpdate(Map<Id, sObject> updatedRecordsMap, Map<Id, sObject> oldRecordsMap) Parameters updatedRecordsMap Type: Map<Id, sObject> Map of IDs to updated trailheadapp__Badge__c records oldRecordsMap Type: Map<Id, sObject> Map of IDs to old trailheadapp__Badge__c records"
     },
     {
         "title": "TrailTriggerHandler",
         "fileName": "TrailTriggerHandler.html",
-        "text": "TrailTriggerHandler : Signature public without sharing class TrailTriggerHandler  extends TriggerHandler Author : Ben Learn TrailTriggerHandler Constructors TrailTriggerHandler() TrailTriggerHandler() Signature public TrailTriggerHandler() TrailTriggerHandler Methods afterUpdate(updatedRecordsMap, oldRecordsMap) afterUpdate(updatedRecordsMap, oldRecordsMap) Signature protected override void afterUpdate(Map<Id, sObject> updatedRecordsMap, Map<Id, sObject> oldRecordsMap)"
+        "text": "TrailTriggerHandler Trigger handler for trailheadapp__Trail__c custom object Signature public without sharing class TrailTriggerHandler  extends TriggerHandler Author Ben Learn Since 06-02-2023 TrailTriggerHandler Constructors TrailTriggerHandler() TrailTriggerHandler() Signature public TrailTriggerHandler() TrailTriggerHandler Methods afterUpdate(updatedRecordsMap, oldRecordsMap) afterUpdate(updatedRecordsMap, oldRecordsMap) Signature protected override void afterUpdate(Map<Id, sObject> updatedRecordsMap, Map<Id, sObject> oldRecordsMap) Parameters updatedRecordsMap Type: Map<Id, sObject> Map of IDs to updated trailheadapp__Trail__c records oldRecordsMap Type: Map<Id, sObject> Map of IDs to old trailheadapp__Trail__c records"
     },
     {
         "title": "TrailheadEntities",
         "fileName": "TrailheadEntities.html",
-        "text": "TrailheadEntities : Signature public inherited sharing class TrailheadEntities Author : Ben Learn TrailheadEntities Properties Name Signature entityType private String entityType namespacePrefix private String namespacePrefix oldRecordsMap private Map<Id, sObject> oldRecordsMap trackedFieldsByObject private static Map<String, List<String>> trackedFieldsByObject updatedRecordsMap private Map<Id, sObject> updatedRecordsMap TrailheadEntities Constructors TrailheadEntities(updatedRecordsMap, oldRecordsMap, entityType) TrailheadEntities(updatedRecordsMap, oldRecordsMap, entityType) Signature public TrailheadEntities(Map<Id, sObject> updatedRecordsMap, Map<Id, sObject> oldRecordsMap, String entityType) TrailheadEntities Methods getValueIfChanged(updatedRecord, oldRecord, fieldName) publishUpdateEvents() getValueIfChanged(updatedRecord, oldRecord, fieldName) Signature private static Object getValueIfChanged(sObject updatedRecord, sObject oldRecord, String fieldName) publishUpdateEvents() Signature public void publishUpdateEvents()"
+        "text": "TrailheadEntities Update this description Signature public inherited sharing class TrailheadEntities Author Ben Learn Since 06-02-2023 TrailheadEntities Constructors TrailheadEntities(updatedRecordsMap, oldRecordsMap, entityType) TrailheadEntities(updatedRecordsMap, oldRecordsMap, entityType) Signature public TrailheadEntities(Map<Id, sObject> updatedRecordsMap, Map<Id, sObject> oldRecordsMap, String entityType) TrailheadEntities Methods publishUpdateEvents() publishUpdateEvents() Signature public void publishUpdateEvents()"
     },
     {
         "title": "TrailheadEntityUpdateEventTriggerHandler",
         "fileName": "TrailheadEntityUpdateEventTriggerHandler.html",
-        "text": "TrailheadEntityUpdateEventTriggerHandler Trigger handler for the TrailheadEntityUpdateEvent__e platform event Signature public without sharing class TrailheadEntityUpdateEventTriggerHandler extends TriggerHandler Author Ben Learn Since 06-02-2023 TrailheadEntityUpdateEventTriggerHandler Constructors TrailheadEntityUpdateEventTriggerHandler() TrailheadEntityUpdateEventTriggerHandler() Signature public TrailheadEntityUpdateEventTriggerHandler() TrailheadEntityUpdateEventTriggerHandler Methods afterInsert(newRecordsMap) afterInsert(newRecordsMap) Signature protected override void afterInsert(Map<Id, sObject> newRecordsMap) Parameters newRecordsMap Type: Map<Id, sObject>"
+        "text": "TrailheadEntityUpdateEventTriggerHandler Trigger handler for the TrailheadEntityUpdateEvent__e platform event Signature public without sharing class TrailheadEntityUpdateEventTriggerHandler extends TriggerHandler Author Ben Learn Since 06-02-2023 TrailheadEntityUpdateEventTriggerHandler Constructors TrailheadEntityUpdateEventTriggerHandler() TrailheadEntityUpdateEventTriggerHandler() Signature public TrailheadEntityUpdateEventTriggerHandler() TrailheadEntityUpdateEventTriggerHandler Methods afterInsert(newRecordsMap) afterInsert(newRecordsMap) Signature protected override void afterInsert(Map<Id, sObject> newRecordsMap) Parameters newRecordsMap Type: Map<Id, sObject> Map of IDs to newly inserted TrailheadEntityUpdateEvent__e events"
     }
 ];
