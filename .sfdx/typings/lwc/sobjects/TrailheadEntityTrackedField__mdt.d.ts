7e67f143ab0513fa31cc7277473689c131725c0d
Ben <blearn5241@gmail.com>
2024-06-01 09:06:07 -0400

    added UpdatedFields__c and EntityType__c fields on TrailheadEntityUpdateEvent__e and TrailheadEntityUpdate__c. updated Badge and Trail trigger code to populate fields #1

---
+++ .sfdx/typings/lwc/sobjects/TrailheadEntityTrackedField__mdt.d.ts
@@ -0,0 +1,44 @@
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.DeveloperName" {
+  const DeveloperName:string;
+  export default DeveloperName;
+}
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.MasterLabel" {
+  const MasterLabel:string;
+  export default MasterLabel;
+}
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.Language" {
+  const Language:string;
+  export default Language;
+}
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.NamespacePrefix" {
+  const NamespacePrefix:string;
+  export default NamespacePrefix;
+}
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.Label" {
+  const Label:string;
+  export default Label;
+}
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.QualifiedApiName" {
+  const QualifiedApiName:string;
+  export default QualifiedApiName;
+}
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.BadgeField__c" {
+  const BadgeField__c:boolean;
+  export default BadgeField__c;
+}
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.Suffix__c" {
+  const Suffix__c:string;
+  export default Suffix__c;
+}
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.TrailField__c" {
+  const TrailField__c:boolean;
+  export default TrailField__c;
+}
+declare module "@salesforce/schema/TrailheadEntityTrackedField__mdt.Testr__c" {
+  const Testr__c:string;
+  export default Testr__c;
+}
